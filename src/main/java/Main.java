06704d6b430b1d76080fa92aded8293178b8f1cd
Ilker Yildirim <ITSIlkerSE@gmail.com>
2022-09-07 11:40:53 +0200

    Added ArrayList and put all students as objects in.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,8 @@ import model.Citizen;
 import model.Student;
 import model.ComputerScienceStudent;
 
+import java.util.ArrayList;
+
 
 public class Main {
 
@@ -11,11 +13,17 @@ public class Main {
 
         Student myStudent0 = new ComputerScienceStudent("32134", "Ilker", "german", "Hamburg", 72248);
         Student myStudent1 = new ComputerScienceStudent("52414", "Jonas", "english", "Flensburg", 424151);
-
         Student myStudent3 = new BiologyStudent("2313", "Peter", "C++", 231314);
 
-        System.out.println(myStudent3);
-        System.out.println(myStudent0.getCourse());
+        ArrayList studentList = new ArrayList();
+
+        studentList.add(myStudent0);
+        studentList.add(myStudent1);
+        studentList.add(myStudent3);
+
+        System.out.println(studentList.get(0));
+        System.out.println(studentList.get(1));
+        System.out.println(studentList.get(2));
 
 
     }
